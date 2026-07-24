41b4887c6f25d7c9aee20c30846bc30d9a1f4c4b
justinr <justin@apsis.sh>
2026-07-24 11:45:25 -0700

    remove redundant keycaps

--- keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/custom/keymap.c
@@ -238,7 +238,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
         KC_NO,    KC_SCLN,  KC_1,    KC_2,    KC_3, KC_BSLS,      KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                    KC_DOT,   KC_0,  KC_GRV,      KC_NO,  KC_TRNS,
+                                    KC_GRV,   KC_0,  KC_NO,      KC_NO,  KC_TRNS,
                                            KC_NO,     KC_NO,      KC_NO
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
@@ -247,13 +247,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
        KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-       KC_NO,  KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,      KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
+       KC_NO,  KC_LCBR, KC_LPRN, KC_ASTR, KC_RPRN, KC_RCBR,      KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_NO,  KC_QUES,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,      KC_NO,   MOD_H,   MOD_A,   MOD_E,  MOD_I,  KC_NO,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_NO,  KC_COLN, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,      KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                  KC_LPRN, KC_RPRN, KC_TILD,      KC_TRNS,  KC_NO,
+                                  KC_TILD, KC_AMPR,   KC_NO,      KC_TRNS,  KC_NO,
                                            KC_NO,     KC_NO,      KC_NO
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
